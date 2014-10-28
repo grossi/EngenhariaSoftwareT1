5530a85fcdfe33192cfec89080bd969291a0f91e
Rômulo Dusi <dusiromulo@gmail.com>
2014-10-28 10:50:47 -0200

    Inserção da classe Projeto

---
+++ 1213094/src/Project.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+
+public class Project extends JPanel{
+	
+	private ArrayList<ViewCasoDeUso> useCases = new ArrayList<ViewCasoDeUso>();
+	
+	public Project(int id){
+		
+	}
+}
